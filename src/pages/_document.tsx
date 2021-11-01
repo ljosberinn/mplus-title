1ef88115cefd7346a1a889c03e8248d4fa54ede1
Gerrit Alex <admin@gerritalex.de>
2021-11-01 13:39:26 +0100

    refactor: add opengraph tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,48 @@ export default function CustomDocument(/* props: DocumentProps*/): JSX.Element {
         <meta content="global" name="distribution" />
         <meta content="7 days" name="revisit-after" />
         <meta content="Gerrit Alex" name="author" />
+
+        <meta itemProp="name" content="Mythic+ Estimated Title Cutoff" />
+        <meta
+          itemProp="description"
+          content="Displays estimated rank & score required for the seasonal Mythic+ title."
+        />
+        <meta
+          property="image:alt"
+          content="Mythic+ Estimated Title Cutoff mascot Froggy"
+        />
+
+        <meta property="og:url" content="https://mplus-title.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Mythic+ Estimated Title Cutoff" />
+        <meta
+          property="og:image:alt"
+          content="Mythic+ Estimated Title Cutoff"
+        />
+        <meta
+          property="og:description"
+          content="Displays estimated rank & score required for the seasonal Mythic+ title."
+        />
+        <meta
+          property="og:site_name"
+          content="Mythic+ Estimated Title Cutoff"
+        />
+        <meta property="og:locale" content="en_US" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@gerrit_alex" />
+        <meta name="twitter:url" content="https://mplus-title.vercel.app/" />
+        <meta name="twitter:title" content="Mythic+ Estimated Title Cutoff" />
+        <meta
+          name="twitter:description"
+          content="Displays estimated rank & score required for the seasonal Mythic+ title."
+        />
+        <meta
+          name="twitter:image:alt"
+          content="Mythic+ Estimated Title Cutoff"
+        />
       </Head>
-      <body className="bg-gray-900 text-gray-200">
+      <body className="text-gray-200 bg-gray-900">
         <Main />
         <NextScript />
       </body>
