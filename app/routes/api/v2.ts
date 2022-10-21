f2cca1a5282623ca875757b31456fa644eefefed
Gerrit Alex <admin@gerritalex.de>
2022-10-21 03:24:37 +0200

    adapt api to do nothing during offseason

--- app/routes/api/v2.ts
+++ app/routes/api/v2.ts
@@ -3,8 +3,8 @@ import type { Prisma, Regions } from "@prisma/client";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { load } from "cheerio";
-import { seasonStartDates } from "~/meta";
 import { prisma } from "~/prisma";
+import { determineCurrentSeason } from "~/seasons";
 
 const rioBaseUrl = "https://raider.io";
 
@@ -24,20 +24,6 @@ if (!String.prototype.replaceAll) {
     return this.replace(new RegExp(str, "gu"), newStr);
   };
 }
-
-const determineSeason = (region: Regions) => {
-  const firstMatch = Object.entries(seasonStartDates).find(
-    ([, regionStartMap]) => {
-      return Date.now() >= regionStartMap[region];
-    }
-  );
-
-  if (!firstMatch) {
-    throw new Error("could not determine current season");
-  }
-
-  return firstMatch[0].replace("-season", "");
-};
 
 export const action: ActionFunction = async ({ request }) => {
   if (request.method !== "POST") {
@@ -64,6 +50,13 @@ export const action: ActionFunction = async ({ request }) => {
       console.info("Skipping verification of secret.", { payload });
     }
 
+    const now = Date.now();
+    const season = determineCurrentSeason(now);
+
+    if (!season) {
+      return json({ info: "No ongoing season, bailing." });
+    }
+
     console.time("getMostOutdatedRegion");
     const mostOutdatedRegion = await getMostOutdatedRegion();
     console.timeEnd("getMostOutdatedRegion");
@@ -74,7 +67,7 @@ export const action: ActionFunction = async ({ request }) => {
     }
 
     console.time("parseRegionData");
-    const regionData = await parseRegionData(mostOutdatedRegion ?? "us");
+    const regionData = await parseRegionData(mostOutdatedRegion, season.rioKey);
     console.timeEnd("parseRegionData");
 
     await prisma.crossFactionHistory.create({ data: regionData });
@@ -146,47 +139,119 @@ const getMostOutdatedRegion = async () => {
   return mostOutdated.region;
 };
 
-const createPageUrl = (region: Regions, page = 0) => {
-  const season = determineSeason(region);
-  return `${rioBaseUrl}/mythic-plus-character-rankings/season-${season}/${region}/all/all/${page}`;
+const createPageUrl = (rioSeasonName: string, region: Regions, page = 0) => {
+  return `${rioBaseUrl}/mythic-plus-character-rankings/${rioSeasonName}/${region}/all/all/${page}`;
 };
 
-const retrieveLastPageUrl = async (region: Regions) => {
-  const firstPageUrl = createPageUrl(region);
+type LastPageUrlParams = {
+  url: string;
+  page: number;
+  initialPage: number;
+};
+
+const retrieveLastPageUrl = async (
+  rioSeasonName: string,
+  region: Regions
+): Promise<LastPageUrlParams> => {
+  const firstPageUrl = createPageUrl(rioSeasonName, region);
   const firstPageResponse = await fetch(firstPageUrl);
   const firstPageText = await firstPageResponse.text();
 
   const $firstPage = load(firstPageText);
 
-  return $firstPage(".rio-pagination--button").last().attr("href");
+  const url = $firstPage(".rio-pagination--button").last().attr("href");
+
+  if (!url) {
+    return {
+      url: "",
+      initialPage: 0,
+      page: 0,
+    };
+  }
+
+  const withoutHash = url.includes("#content")
+    ? url.replace("#content", "")
+    : url;
+
+  const { page, url: urlWithoutPage } = parsePage(withoutHash);
+
+  return {
+    url: urlWithoutPage,
+    page,
+    initialPage: page,
+  };
 };
 
-const determineLastEligibleRank = async (lastPageUrl: string) => {
-  const lastPageResponse = await fetch(`${rioBaseUrl}${lastPageUrl}`);
+const parsePage = (str: string) => {
+  const parts = str.split("/");
+
+  return {
+    page: Number.parseInt(parts[parts.length - 1]),
+    url: parts.slice(0, -1).join("/"),
+  };
+};
+
+const determineLastEligibleRank = async (
+  lastPageParams: LastPageUrlParams
+): Promise<number> => {
+  const retryDiff = lastPageParams.initialPage - lastPageParams.page;
+
+  if (retryDiff === 3) {
+    console.debug("too many retries to determine last eligible rank, bailing");
+    return 0;
+  }
+
+  const url = `${rioBaseUrl}${lastPageParams.url}/${lastPageParams.page}`;
+  const lastPageResponse = await fetch(url);
   const lastPageText = await lastPageResponse.text();
   const $lastPage = load(lastPageText);
 
   const cellSelector =
     ".mythic-plus-rankings--row:last-of-type .rank-text-normal";
 
+  const textContent = $lastPage(cellSelector).text();
+
   const totalRankedCharacters = Number.parseInt(
-    $lastPage(cellSelector).text().replaceAll(",", "")
+    textContent.replaceAll(",", "")
   );
+
+  if (Number.isNaN(totalRankedCharacters)) {
+    const prevPage = lastPageParams.page - 1;
+
+    if (prevPage === 0) {
+      console.debug("probably no entries yet, bailing");
+      return 0;
+    }
+
+    return determineLastEligibleRank({
+      ...lastPageParams,
+      page: prevPage,
+    });
+  }
+
+  console.debug({ totalRankedCharacters });
 
   return Math.floor(totalRankedCharacters * 0.001);
 };
 
-const retrieveScore = async (region: Regions, lastEligibleRank: number) => {
+const retrieveScore = async (
+  rioSeasonName: string,
+  region: Regions,
+  lastEligibleRank: number
+) => {
   const scorePage =
     lastEligibleRank <= 20 ? 0 : Math.floor(lastEligibleRank / 20);
 
   const scorePageUrl = createPageUrl(
+    rioSeasonName,
     region, // if rank is divisible by 20, e.g. 80, it would result in page 4
     // but its still on page 3
     lastEligibleRank % 20 === 0 && lastEligibleRank > 20
       ? scorePage - 1
       : scorePage
   );
+
+  console.debug({ scorePageUrl });
 
   const scorePageResponse = await fetch(scorePageUrl);
   const scorePageText = await scorePageResponse.text();
@@ -210,15 +275,17 @@ const retrieveScore = async (region: Regions, lastEligibleRank: number) => {
 };
 
 const parseRegionData = async (
-  region: Regions
+  region: Regions,
+  rioSeasonName: string
 ): Promise<Prisma.CrossFactionHistoryCreateInput> => {
   const now = Math.round(Date.now() / 1000);
 
   console.time("retrieveLastPageUrl");
-  const lastPageUrl = await retrieveLastPageUrl(region);
+  const lastPageUrlParams = await retrieveLastPageUrl(rioSeasonName, region);
   console.timeEnd("retrieveLastPageUrl");
+  console.debug({ lastPageUrlParams });
 
-  if (!lastPageUrl) {
+  if (!lastPageUrlParams.url) {
     console.warn("Could not parse last page button, bailing without data.");
     return {
       score: 0,
@@ -229,10 +296,21 @@ const parseRegionData = async (
   }
 
   console.time("determineLastEligibleRank");
-  const lastEligibleRank = await determineLastEligibleRank(lastPageUrl);
+  const lastEligibleRank = await determineLastEligibleRank(lastPageUrlParams);
   console.timeEnd("determineLastEligibleRank");
+
+  if (lastEligibleRank === 0) {
+    console.warn("Could not parse last eligible rank, bailing without data.");
+    return {
+      score: 0,
+      rank: 0,
+      timestamp: now,
+      region,
+    };
+  }
+
   console.time("retrieveScore");
-  const score = await retrieveScore(region, lastEligibleRank);
+  const score = await retrieveScore(rioSeasonName, region, lastEligibleRank);
   console.timeEnd("retrieveScore");
 
   return {
