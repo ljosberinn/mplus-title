d02724062db53c805c91d40cd4e335590a6cee90
Gerrit Alex <admin@gerritalex.de>
2022-10-21 03:23:58 +0200

    start rebuilding seasonal data

---
+++ app/seasons.ts
@@ -0,0 +1,263 @@
+import type { Factions, Regions } from "@prisma/client";
+
+type CutoffSource = { score: number; source: string | null };
+
+enum Affix {
+  Overflowing = 1,
+  Skittish = 2,
+  Volcanic = 3,
+  Necrotic = 4,
+  Teeming = 5,
+  Raging = 6,
+  Bolstering = 7,
+  Sanguine = 8,
+  Tyrannical = 9,
+  Fortified = 10,
+  Bursting = 11,
+  Grievous = 12,
+  Explosive = 13,
+  Quaking = 14,
+  Infested = 16,
+  Reaping = 117,
+  Beguiling = 119,
+  Awakened = 120,
+  Prideful = 121,
+  Inspiring = 122,
+  Spiteful = 123,
+  Storming = 124,
+  Tormented = 128,
+  Infernal = 129,
+  Encrypted = 130,
+  Shrouded = 131,
+  Thundering = 132,
+}
+
+const UNKNOWN_SEASON_ENDING = 0;
+
+export type Season = {
+  name: string;
+  slug: string;
+  startDates: Record<Regions, number>;
+  endDates: Record<Regions, number>;
+  confirmedCutoffs: Record<
+    Regions,
+    CutoffSource | Record<Factions, CutoffSource>
+  >;
+  affixes: [Affix, Affix, Affix, Affix][];
+  rioKey: string;
+  hasCrossFactionSupport: boolean;
+};
+
+const seasons: Season[] = [
+  {
+    name: "DF S1 - Thundering",
+    slug: "df-season-1",
+    rioKey: "season-df-1",
+    hasCrossFactionSupport: true,
+    startDates: {
+      us: 1_670_943_600_000,
+      eu: 1_671_001_200_000,
+      kr: 1_671_058_800_000,
+      tw: 1_671_058_800_000,
+    },
+    endDates: {
+      us: UNKNOWN_SEASON_ENDING,
+      eu: UNKNOWN_SEASON_ENDING,
+      kr: UNKNOWN_SEASON_ENDING,
+      tw: UNKNOWN_SEASON_ENDING,
+    },
+    confirmedCutoffs: {
+      eu: { score: 0, source: null },
+      us: { score: 0, source: null },
+      kr: { score: 0, source: null },
+      tw: { score: 0, source: null },
+    },
+    affixes: [],
+  },
+  {
+    name: "SL S4 - Shrouded",
+    slug: "sl-season-4",
+    rioKey: "season-sl-4",
+    hasCrossFactionSupport: true,
+    startDates: {
+      us: 1_659_452_400_000,
+      eu: 1_659_510_000_000,
+      kr: 1_659_564_000_000,
+      tw: 1_659_564_000_000,
+    },
+    endDates: {
+      us: 1_666_710_000_000,
+      eu: 1_666_767_600_000,
+      kr: 1_666_825_200_000,
+      tw: 1_666_825_200_000,
+    },
+    confirmedCutoffs: {
+      eu: { score: 0, source: null },
+      us: { score: 0, source: null },
+      kr: { score: 0, source: null },
+      tw: { score: 0, source: null },
+    },
+    affixes: [
+      [Affix.Tyrannical, Affix.Inspiring, Affix.Quaking, Affix.Thundering],
+      [Affix.Fortified, Affix.Sanguine, Affix.Grievous, Affix.Thundering],
+      [Affix.Tyrannical, Affix.Bolstering, Affix.Explosive, Affix.Thundering],
+      [Affix.Fortified, Affix.Bursting, Affix.Storming, Affix.Thundering],
+      [Affix.Tyrannical, Affix.Raging, Affix.Volcanic, Affix.Thundering],
+      [Affix.Fortified, Affix.Inspiring, Affix.Grievous, Affix.Thundering],
+      [Affix.Tyrannical, Affix.Spiteful, Affix.Necrotic, Affix.Thundering],
+      [Affix.Fortified, Affix.Bolstering, Affix.Quaking, Affix.Thundering],
+      [Affix.Tyrannical, Affix.Sanguine, Affix.Storming, Affix.Thundering],
+      [Affix.Fortified, Affix.Raging, Affix.Explosive, Affix.Thundering],
+      [Affix.Tyrannical, Affix.Bursting, Affix.Volcanic, Affix.Thundering],
+      [Affix.Fortified, Affix.Spiteful, Affix.Necrotic, Affix.Thundering],
+    ],
+  },
+  {
+    name: "SL S3 - Encrypted",
+    slug: "sl-season-3",
+    rioKey: "season-sl-3",
+    hasCrossFactionSupport: true,
+    startDates: {
+      us: 1_646_146_800_000,
+      eu: 1_646_204_400_000,
+      kr: 1_646_258_400_000,
+      tw: 1_646_258_400_000,
+    },
+    endDates: {
+      us: 1_659_452_400_000,
+      eu: 1_659_510_000_000,
+      kr: 1_659_564_000_000,
+      tw: 1_659_564_000_000,
+    },
+    confirmedCutoffs: {
+      eu: {
+        score: 3725,
+        source:
+          "https://eu.forums.blizzard.com/en/wow/t/m-cryptic-hero-title-score-updated-daily/371434",
+      },
+      us: { score: 0, source: null },
+      kr: { score: 0, source: null },
+      tw: { score: 0, source: null },
+    },
+    affixes: [
+      [Affix.Tyrannical, Affix.Bolstering, Affix.Explosive, Affix.Encrypted],
+      [Affix.Fortified, Affix.Bursting, Affix.Storming, Affix.Encrypted],
+      [Affix.Tyrannical, Affix.Raging, Affix.Volcanic, Affix.Encrypted],
+      [Affix.Fortified, Affix.Inspiring, Affix.Grievous, Affix.Encrypted],
+      [Affix.Tyrannical, Affix.Spiteful, Affix.Necrotic, Affix.Encrypted],
+      [Affix.Fortified, Affix.Bolstering, Affix.Quaking, Affix.Encrypted],
+      [Affix.Tyrannical, Affix.Sanguine, Affix.Storming, Affix.Encrypted],
+      [Affix.Fortified, Affix.Raging, Affix.Explosive, Affix.Encrypted],
+      [Affix.Tyrannical, Affix.Bursting, Affix.Volcanic, Affix.Encrypted],
+      [Affix.Fortified, Affix.Spiteful, Affix.Necrotic, Affix.Encrypted],
+      [Affix.Tyrannical, Affix.Inspiring, Affix.Quaking, Affix.Encrypted],
+      [Affix.Fortified, Affix.Sanguine, Affix.Grievous, Affix.Encrypted],
+    ],
+  },
+  {
+    name: "SL S2 - Tormented",
+    slug: "sl-season-2",
+    rioKey: "season-sl-2",
+    hasCrossFactionSupport: false,
+    startDates: {
+      us: 1_625_583_600_000,
+      eu: 1_625_641_200_000,
+      kr: 1_625_695_200_000,
+      tw: 1_625_695_200_000,
+    },
+    endDates: {
+      us: 1_645_542_000_000,
+      eu: 1_645_599_600_000,
+      kr: 1_645_653_600_000,
+      tw: 1_645_653_600_000,
+    },
+    confirmedCutoffs: {
+      eu: {
+        alliance: {
+          score: 2788,
+          source:
+            "https://eu.forums.blizzard.com/en/wow/t/m-tormented-hero-title-score-updated-daily/341108",
+        },
+        horde: {
+          score: 2875,
+          source:
+            "https://eu.forums.blizzard.com/en/wow/t/m-tormented-hero-title-score-updated-daily/341108",
+        },
+      },
+      us: {
+        alliance: {
+          score: 2768,
+          source:
+            "https://us.forums.blizzard.com/en/wow/t/m-tormented-hero-title-score-updated-daily/1184111",
+        },
+        horde: {
+          score: 2847,
+          source:
+            "https://us.forums.blizzard.com/en/wow/t/m-tormented-hero-title-score-updated-daily/1184111",
+        },
+      },
+      kr: {
+        alliance: {
+          score: 0,
+          source: null,
+        },
+        horde: {
+          score: 0,
+          source: null,
+        },
+      },
+      tw: {
+        alliance: {
+          score: 0,
+          source: null,
+        },
+        horde: {
+          score: 0,
+          source: null,
+        },
+      },
+    },
+    affixes: [
+      [Affix.Tyrannical, Affix.Bursting, Affix.Volcanic, Affix.Tormented],
+      [Affix.Fortified, Affix.Inspiring, Affix.Grievous, Affix.Tormented],
+      [Affix.Tyrannical, Affix.Spiteful, Affix.Necrotic, Affix.Tormented],
+      [Affix.Fortified, Affix.Bolstering, Affix.Quaking, Affix.Tormented],
+      [Affix.Tyrannical, Affix.Sanguine, Affix.Storming, Affix.Tormented],
+      [Affix.Fortified, Affix.Raging, Affix.Explosive, Affix.Tormented],
+      [Affix.Tyrannical, Affix.Bursting, Affix.Volcanic, Affix.Tormented],
+      [Affix.Fortified, Affix.Necrotic, Affix.Grievous, Affix.Tormented],
+      [Affix.Tyrannical, Affix.Inspiring, Affix.Quaking, Affix.Tormented],
+      [Affix.Fortified, Affix.Sanguine, Affix.Necrotic, Affix.Tormented],
+      [Affix.Tyrannical, Affix.Bolstering, Affix.Explosive, Affix.Tormented],
+      [Affix.Fortified, Affix.Bursting, Affix.Storming, Affix.Tormented],
+    ],
+  },
+];
+
+export const hasSeasonEndedForAllRegions = (slug: string): boolean => {
+  const season = seasons.find((season) => season.slug === slug);
+
+  if (!season) {
+    return true;
+  }
+
+  const endDates = Object.values(season.endDates);
+
+  if (endDates.includes(UNKNOWN_SEASON_ENDING)) {
+    return false;
+  }
+
+  const now = Date.now();
+
+  return endDates.every((date) => now >= date);
+};
+
+export const determineCurrentSeason = (timestamp: number): Season | null => {
+  const season = seasons.find(
+    (season) =>
+      Object.values(season.startDates).some((start) => timestamp >= start) &&
+      Object.values(season.endDates).some((end) => end === 0 || end > timestamp)
+  );
+
+  return season ?? null;
+};
