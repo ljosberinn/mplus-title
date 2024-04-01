e93c99c8b9b97c0c1a9c0cde1baa3b2ececba4b6
Gerrit Alex <admin@gerritalex.de>
2024-04-01 02:25:29 +0200

    lazy load custom extrapolation form

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,7 @@
+import { lazy, Suspense } from "react";
+
 import { type EnhancedSeason } from "~/seasons";
 
-import { CustomExtrapolationForm } from "./CustomExtrapolationForm";
 import { Logo } from "./Logo";
 import { BuyMeACoffee, RaiderPatreon, Twitter, WCLPatreon } from "./NavLink";
 import { OverlaysToggle } from "./OverlaysToggle";
@@ -11,7 +12,17 @@ type HeaderProps = {
   season: EnhancedSeason;
 };
 
+const CustomExtrapolationForm = lazy(() => import("./CustomExtrapolationForm"));
+
 export function Header({ season }: HeaderProps): JSX.Element {
+  const seasonHasStarted = Object.values(season.startDates).some(
+    (maybeDate) => maybeDate !== null && maybeDate <= Date.now(),
+  );
+
+  const seasonHasEndedInEveryRegion = Object.values(season.endDates).every(
+    (maybeDate) => maybeDate !== null && maybeDate <= Date.now(),
+  );
+
   return (
     <>
       <header className="flex h-20 items-center justify-between border-b  border-gray-700 p-6 text-stone-100 drop-shadow-sm print:hidden">
@@ -36,11 +47,15 @@ export function Header({ season }: HeaderProps): JSX.Element {
           <OverlaysToggle season={season} />
         </div>
       </div>
-      <div className="mx-auto flex w-full max-w-screen-2xl items-center justify-between">
-        <div className="flex w-full flex-col flex-wrap justify-between gap-3 md:flex-row">
-          <CustomExtrapolationForm season={season} />
-        </div>
-      </div>
+      {seasonHasStarted && !seasonHasEndedInEveryRegion ? (
+        <Suspense fallback={null}>
+          <div className="mx-auto flex w-full max-w-screen-2xl items-center justify-between">
+            <div className="flex w-full flex-col flex-wrap justify-between gap-3 md:flex-row">
+              <CustomExtrapolationForm season={season} />
+            </div>
+          </div>
+        </Suspense>
+      ) : null}
     </>
   );
 }
