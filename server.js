00b3043b7a6ba3d09d4d1aff99ac466ec2e2796c
Gerrit Alex <admin@gerritalex.de>
2025-12-24 00:49:34 +0100

    remove leftover files

--- server.js
+++
@@ -1,6 +0,0 @@
-import * as build from "@remix-run/dev/server-build";
-import { createRequestHandler } from "@remix-run/vercel";
-
-import { env } from "./app/env/server";
-
-export default createRequestHandler({ build, mode: env.NODE_ENV });
